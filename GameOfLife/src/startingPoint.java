6a6e1c143b855b6e5ad0d3660549e5a5d79e154a
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2019-02-05 23:13:18 +0000

    Edit

--- GameOfLife/src/startingPoint.java
+++ GameOfLife/src/startingPoint.java
@@ -14,7 +14,7 @@ public class startingPoint {
 	public static void main(String[] args) {
 		// console output of initialization
 		System.out.println("Game of Life initialized");
-		// system exiting normally with no error
+		// system exiting normally with no errors
 		System.exit(0);
 	}
 
