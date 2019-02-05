4210d893b2fff34233927dc3fb058afbf76e224a
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2019-02-05 23:09:53 +0000

    Eclipse Code started and integration

---
+++ GameOfLife/src/startingPoint.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kinshuk
+ *
+ */
+public class startingPoint {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// console output of initialization
+		System.out.println("Game of Life initialized");
+		// system exiting normally with no error
+		System.exit(0);
+	}
+
+}
